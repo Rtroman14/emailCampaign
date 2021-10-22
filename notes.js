8cff0cebb3bf906bd38d83350a5e57852af1cd1f
Ryan Roman <rtroman14@gmail.com>
2021-10-22 09:20:12 -0600

    update

---
+++ notes.js
@@ -0,0 +1,10 @@
+// get all accounts
+// create map of async functions
+
+// async function:
+// get 20 prospects from AT
+// push all prospects to HL -- Promise.all()?
+// update all records -- batch - (https://airtable.com/apps7T6bpqSy7XOfa/api/docs#javascript/table:prospects:update)
+// return true or false depending on outcome
+
+// with response of all async functions, loop over and update AT clients
